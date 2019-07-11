a1a290810be6c795346c44e0744b6c43bb61b8c7
JASON ROUNDTREE <jasontree123@gmail.com>
2019-07-10 19:39:55 -0700

    Initial commit

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,30 @@
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions/schemas
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+    }
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my first query'
+        },
+        name() {
+            return 'Jason'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('Server is running')
+})

dafcef9954259f87261511763b4b97d5456908b5
JASON ROUNDTREE <jasontree123@gmail.com>
2019-07-19 19:10:39 -0700

    Completed section 2

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -2,14 +2,46 @@ import { GraphQLServer } from 'graphql-yoga'
 
 // Scalar types: String, Boolean, Int, Float, ID
 
+// Demo user data
+const users = [{
+    id: '1',
+    name: 'Joey John-John',
+    email: 'joey@shabalagoo.john',
+}, {
+    id: '2',
+    name: 'Barney Gumble',
+    email: 'bgumble@moesbar.com',
+}]
+const posts = [{
+    id: 9,
+    title: 'Post with the Most',
+    body: 'dsfj ldsjf kjsdf sdj',
+    published: true,
+    author: '1'
+}, {
+    id: 8,
+    title: 'Youhklih Bojh',
+    body: 'dsf ijfsdjlk lhjjdsf',
+    published: false,
+    author: '2'
+}]
+const comments = [{
+    id: 1,
+    text: 'dakfjdl lksdfj dsjflksdj',
+    author: '1',
+    post: 9
+}, {
+    id: 2,
+    text: 'rweuwe uhkj hkyuioy uy uiyo',
+    author: '2',
+    post: 9
+}]
 // Type definitions/schemas
 const typeDefs = `
     type Query {
         users(query: String): [User!]!
         posts(query: String): [Post!]!
-        greeting(name: String): String!
-        add(grades: [Int!]!): Int!
-        grades: [Int!]!
+        comments: [Comment!]!
         me: User!
         post: Post!
     }
@@ -18,82 +50,85 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
     }
     type Post {
         id: ID!
         title: String!
         body: String!
         published: Boolean!
+        author: User!
+        comments: [Comment!]!
     }
+    type Comment {
+        id: ID!
+        text: String!
+        author: User!
+        post: Post!
+    }
+    
 `
-// Demo user data
-const users = [{
-    id: '1',
-    name: 'Joey John-John',
-    email: 'joey@shabalagoo.john'
-}, {
-    id: '2',
-    name: 'Barney Gumble',
-    email: 'bgumble@moesbar.com'
-}]
-const posts = [{
-    id: 9,
-    title: 'Post with the Most',
-    body: 'dsfj ldsjf kjsdf sdj',
-    published: true 
-}, {
-    id: 8,
-    title: 'Youhklih Bojh',
-    body: 'dsf ijfsdjlk lhjjdsf',
-    published: false
-}]
+
 // Resolvers
 const resolvers = {
     Query: {
-        posts(parent, args, ctx, info) {
-            if (!args.query) {
-                return posts
-            } else {
-                return posts.filter(post => post.title.toLowerCase().includes(args.query.toLowerCase()))
-            }
-        },
         users(parent, args, ctx, info) {
             if (!args.query) {
                 return users
-            } else {
-                return users.filter(user => user.name.toLowerCase().includes(args.query.toLowerCase()))
             }
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
         },
-        add(parent, args, ctx, info) {
-            if (args.grades.length === 0) {
-                return 0
-            } else {
-                return args.grades.reduce((acc, curr) => acc + curr, 0)
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
             }
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return isTitleMatch || isBodyMatch
+            })
         },
-        greeting(parent, args, ctx, info) {
-            if (args.name) {
-                return `Hello, ${args.name}!`
-            }
-            return 'Hello!'
+        comments(parent, args, ctx, info) {
+            return comments
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
         },
-        grades(parent, args) {
-            return [90, 75, 82]
+        comments(parent, args, ctx, info) {
+            return comments.filter(comment => {
+                return comment.post === parent.id
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter(post => {
+                return post.author === parent.id
+            })
         },
-        me() {
-            return {
-                id: 2,
-                name: 'Jason',
-                email: 'jason@email.xxx',
-            }
+        comments(parent, args, ctx, info) {
+            return comments.filter(comment => {
+                return comment.author === parent.id
+            })
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info) {
+            return users.find(user => {
+                return user.id === parent.author
+            })
         },
-        post() {
-            return {
-                id: 3,
-                title: 'BlahBlah',
-                body: '.....aasfdsafsa',
-                published: true
-            }
+        post(parent, args, ctx, info) {
+            return posts.find(post => {
+                return post.id === parent.post
+            })
         }
     }
 }

798fb2bfdbf1301f47c018d1b49e6ec304df436f
JASON ROUNDTREE <jasontree123@gmail.com>
2019-08-06 20:36:27 -0700

    Completed 5.53

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,12 +5,57 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 })
 
-prisma.query.users(null, '{ id name email posts{ id, title } }')
-    .then((data) => {
-        console.log('users query data: ', JSON.stringify(data, undefined, 2))
-    })
+// const createPostForUser = async (authorId, data) => {
+//     const post = await prisma.mutation.createPost({
+//         data: {
+//             ...data,
+//             author: { 
+//                 connect: {
+//                     id: authorId
+//                 }
+//             }
+//         }
+//     }, '{ id }')
+//     const user = await prisma.query.user({
+//         where: { id: authorId }
+//     }, '{ id name email posts {id title published} }')
+//     return user
+// }
+// createPostForUser('cjyz0bnfj00bf0963zf8m20a3', {
+//     title: 'createPostForUser',
+//     body: 'great createPostForUser!',
+//     published: true
+// }).then((user) => {
+//     console.log('createPostForUser: ', JSON.stringify(user, undefined, 2))
+// })
 
-prisma.query.comments(null, '{ id text author{ id name } }')
-    .then((data) => {
-        console.log('comments query data: ', JSON.stringify(data, undefined, 2))
-    })
+const updatePostForUser = async (postId, data) => {
+    const post = await prisma.mutation.updatePost({
+        where: { id: postId },
+        data
+    }, '{ author {id} }')
+    const user = await prisma.query.user({
+        where: { id: post.author.id },
+    }, '{ id name email posts {id, title, body, published} }')
+    return user
+}
+
+// updatePostForUser('cjz0mk6wt000s09636rr3qqc9', { published: false })
+//     .then((user) => {
+//         console.log(JSON.stringify(user, undefined, 2))
+//     })
+
+// prisma.mutation.updatePost({
+//     where: { id: "cjz0mk6wt000s09636rr3qqc9" },
+//     data: {
+//         body: "Oh my gersh, look at my updated post!",
+//         published: false
+//     }
+// }, '{ id title body published }')
+//     .then((data) => {
+//         console.log('updatePost: ', data)
+//         return prisma.query.posts(null, '{ id, title, body, published }')
+//     })
+//     .then((data) => {
+//         console.log('allPosts: ', data)
+//     })
